855e5cf99223fb0ef3ba897c075ecb4b5bf5bc6f
edwinpaye <baratheonts@gmail.com>
2019-02-20 02:13:22 -0400

    add endpoint p2 post credential

--- src/modules/v1/accounts.routes.js
+++ src/modules/v1/accounts.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { AccountsService } from './services/accounts.service';
 import { SessionService } from './services/session.service';
+import { CredentialsService } from './services/credentials.service';
 import { Session } from '../../components/session';
 
 module.exports = (app) => {
@@ -14,17 +15,16 @@ module.exports = (app) => {
   router.param('account_id', AccountsService.load);
   app.use('/p1', router);
 
-  let routerP = express.Router();
-  // routerP.get('/accounts', AccountsService.query);
-  routerP.route('/accounts/:account_id')
-  .get(AccountsService.show);
-  // .put(AccountsService.update)
-  // .delete(AccountsService.remove);
-  routerP.param('account_id', AccountsService.load);
-  app.use('/p2', routerP);
-
   let routerAuth = express.Router();
   routerAuth.post('/login', SessionService.login);
   routerAuth.post('/logout', Session.auth, SessionService.logout);
   app.use('/p1', routerAuth);
+
+  let routerP = express.Router();
+  routerP.route('/accounts/:account_id')
+  .get(AccountsService.showP);
+  routerP.route('/accounts/:account_id/credentials')
+  .post(CredentialsService.create);
+  routerP.param('account_id', AccountsService.load);
+  app.use('/p2', routerP);
 };
